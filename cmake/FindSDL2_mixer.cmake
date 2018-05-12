ad432a82d03512eb9fe43273a6691970191359c6
MariusMM2 <dmunteanu91@yahoo.com>
2018-05-12 02:20:32 +0200

    working tff and mixer includes

---
+++ cmake/FindSDL2_mixer.cmake
@@ -0,0 +1,3 @@
+#hardcoded workaround
+set(SDL2_TTF_LIBRARIES ${PROJECT_SOURCE_DIR}/SDL2-2.0.8/i686-w64-mingw32/lib/libSDL2_mixer.dll.a)
+set(SDL2_TTF_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/SDL2-2.0.8/i686-w64-mingw32/include/SDL2/SDL_mixer)
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#define SDL_MAIN_HANDLED
+
+#include <string>
+#include "SDL.h"
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+            "SDL2Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //Set default current surface
+    while( !quit )
+    {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            //User requests quit
+            if( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
